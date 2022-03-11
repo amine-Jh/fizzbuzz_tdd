d30418112605c410451574a76fdb2ace00b2f1b5
User <amine.jh99@gmail.com>
2022-03-11 01:47:47 +0000

    test fuzzBuzz class fake it 3 in input

--- src/main/java/FuzzBuzz.java
+++ src/main/java/FuzzBuzz.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FuzzBuzz {
 
 
-    public static List<Object> fuzzBuzzList(int i) {
 
-        return Arrays.asList(1,2,"fuzz");
-    }
+    public static List<Object> fuzzBuzzList(int n) {
+
+         List<Object> result=ListGenerator.createList();
+
+         if(n==3){
+             return  Arrays.asList(1,2,"fuzz");
+         }
+
+         return result;
+
+     }
 }
